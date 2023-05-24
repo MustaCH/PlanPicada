b06a07e5740f1bc0671fadd084281cb1e8433e0e
Musta <polettiignacio7@gmail.com>
2023-05-24 17:55:53 -0300

    [feature] agregado nuevo carousel, falta setear

--- index.js
+++ index.js
@@ -11,3 +11,38 @@ navLinks.forEach(function (navLink) {
     this.classList.add("custom-active");
   });
 });
+
+//CARRUSEL PICADAS
+
+var swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  init: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    620: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
